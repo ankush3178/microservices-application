a271872f481cb1ee102e6ff3c6011596d2507c4a
Ankush Notiyal <ankushnotiyal@Ankushs-MacBook-Air.local>
2025-02-25 14:22:52 +0100

    changes

--- infra/provider.tf
+++ infra/provider.tf
@@ -1,10 +1,3 @@
-terraform {
-  backend "gcs" {
-    bucket  = "your-terraform-state-bucket"
-    prefix  = "terraform/state"
-  }
-}
-
 provider "google" {
   project = "terraform-451704"
   region  = "us-central1"
